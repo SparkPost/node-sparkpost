ee69ab4c33c000ade1f1b37da4c8652c7524daa7
AimeeKnight <aimeemarieknight@gmail.com>
2014-09-18 12:40:53 -0400

    add unit test for #fetch

--- test/spec/transmissions.spec.js
+++ test/spec/transmissions.spec.js
@@ -1,9 +1,9 @@
 var chai = require('chai')
-  , expect = chai.expect;
+  , expect = chai.expect
+  , proxyquire = require('proxyquire');
 
 describe('This is a test', function() {
   var transmission = require('../../lib/transmission');
-
 
   it('should allow for setting values on construction', function() {
     var sdk = new transmission({ campaign: 'camp'});
@@ -113,5 +113,84 @@ describe('This is a test', function() {
     expect(sdk.model.campaign_id).to.equal('camp');
     expect(sdk.model.content.email_rfc822).to.equal('bar');
     expect(sdk.model.content.headers).to.equal('foo');
+  });
+});
+
+describe('#fetch', function() {
+  it('should return error', function(done) {
+    var reqMock = {
+      get: function(url, callbackFunction) {
+        var response = {error: 'test'};
+        callbackFunction(response.error, {statusCode: response.status}, response.body);
+       }
+    }
+
+    var transmission = proxyquire('../../lib/transmission', {
+      'request': reqMock
+    });
+
+    var sdk = new transmission();
+    sdk.fetch(10, function(resp) {
+      expect(resp).to.match(/Unable to contact Transmissions API: test/);
+      done();
+    });
+  });
+
+  it('should return an error if the status code is 404', function(done) {
+    var reqMock = {
+      get: function(url, callbackFunction) {
+        var response = {error: null, status: 404};
+        callbackFunction(response.error, {statusCode: response.status}, response.body);
+       }
+    }
+
+    var transmission = proxyquire('../../lib/transmission', {
+      'request': reqMock
+    });
+
+    var sdk = new transmission();
+    sdk.fetch(10, function(resp) {
+      expect(resp).to.match(/The specified Transmission ID does not exist/);
+      done();
+    });
+  });
+
+  it('should return an error if the response status code is anything other than 200', function(done) {
+    var reqMock = {
+      get: function(url, callbackFunction) {
+        var response = {error: null, status: 500};
+        callbackFunction(response.error, {statusCode: response.status}, response.body);
+       }
+    }
+
+    var transmission = proxyquire('../../lib/transmission', {
+      'request': reqMock
+    });
+
+    var sdk = new transmission();
+    sdk.fetch(10, function(resp) {
+      expect(resp).to.match(/Received bad response from Transmission API: 500/);
+      done();
+    });
+  });
+
+  it('should return a body', function(done) {
+    var reqMock = {
+      get: function(url, callbackFunction) {
+        var response = {error: null, status: 200, body: 'response body'};
+        callbackFunction(response.error, {statusCode: response.status}, response.body);
+       }
+    }
+
+    var transmission = proxyquire('../../lib/transmission', {
+      'request': reqMock
+    });
+
+    var sdk = new transmission();
+    sdk.fetch(10, function(err, body) {
+      expect(err).to.be.null;
+      expect(body).to.match(/response body/);
+      done();
+    });
   });
 });
