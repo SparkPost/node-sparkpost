645fcf6167a64fb0850144a379d19354bd0fc2c3
Jessica Martin <jessica.martin@messagesystems.com>
2014-09-18 14:12:30 -0400

    Added in configuration singleton and updated modules to leverage it

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var Transmission = require('./lib/transmission');
+var config = require('./lib/configuration');
 
-module.exports = function() {
+module.exports = function(globalOpts) {
+  config.setConfig(globalOpts);
+
   return {
     transmission: Transmission
   };
