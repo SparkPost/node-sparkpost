0ee3544e91ed9134c023b1856131d54882e06d3d
Aydrian J. Howard <aydrian@gmail.com>
2015-05-06 12:42:23 -0400

    Added error handling and updated unit tests

--- test/spec/recipientLists.spec.js
+++ test/spec/recipientLists.spec.js
@@ -1,139 +1,102 @@
 var chai = require('chai')
   , expect = chai.expect
   , sinon = require('sinon')
-  , sinonChai = require('sinon-chai')
-  , nock = require('nock')
-  , SparkPost = require('../../lib/index');
+  , sinonChai = require('sinon-chai');
 
 chai.use(sinonChai);
 
 describe('Recipient Lists Library', function() {
   var client, recipientLists;
 
-  before(function () {
-    // setting up a client for all tests to use
-    var key = '12345678901234567890';
+  beforeEach(function() {
+    client = {
+      get: sinon.stub().yields(),
+      post: sinon.stub().yields(),
+      put: sinon.stub().yields(),
+      'delete': sinon.stub().yields()
+    };
 
-    client = new SparkPost(key);
     recipientLists = require('../../lib/recipientLists')(client);
   });
 
-  it('should expose a public all method', function () {
-    expect(recipientLists.all).to.be.a.function;
-  });
-
-  it('should expose a public find method', function () {
-    expect(recipientLists.find).to.be.a.function;
-  });
-
-  it('should expose a public create method', function () {
-    expect(recipientLists.create).to.be.a.function;
-  });
-
-  it('should expose a public delete method', function () {
-    expect(recipientLists['delete']).to.be.a.function;
+  describe('all Method', function() {
+    it('should call client get method with the appropriate uri', function(done) {
+      recipientLists.all(function(err, data) {
+        expect(client.get.firstCall.args[0].uri).to.equal('recipient-lists');
+        done();
+      });
+    });
   });
 
   describe('find Method', function() {
     it('should call client get method with the appropriate uri', function(done) {
-      var requestSpy = sinon.spy(SparkPost.prototype, 'get');
+      var options = {
+        id: 'test'
+      };
+      recipientLists.find(options, function(err, data) {
+        expect(client.get.firstCall.args[0].uri).to.equal('recipient-lists/test');
+        done();
+      });
+    });
 
-      nock('https://api.sparkpost.com')
-        .get('/api/v1/recipient-lists/test')
-        .reply(200, { ok: true });
-
-      recipientLists.find('test', function(err, data) {
-        // need to make sure we called get method
-        expect(requestSpy.calledOnce).to.be.true;
-
-        // making sure the correct uri was constructed
-        expect(data.res.request.uri.href).to.equal('https://api.sparkpost.com:443/api/v1/recipient-lists/test');
-
-        SparkPost.prototype.get.restore(); // restoring function
+    it('should throw an error if id is missing', function(done) {
+      recipientLists.find(null, function(err) {
+        expect(err.message).to.equal('id is required');
+        expect(client.get).not.to.have.been.called;
         done();
       });
     });
 
     it('should allow show_recipients to be set in options', function(done) {
       var options = {
+        id: 'test',
         show_recipients: true
       };
 
-      nock('https://api.sparkpost.com')
-        .get('/api/v1/recipient-lists/test?show_recipients=true')
-        .reply(200, { ok: true });
-
-      recipientLists.find('test', options, function(err, data) {
-        // making sure show_recipients was appended to the querystring
-        expect(data.res.request.uri.href).to.equal('https://api.sparkpost.com:443/api/v1/recipient-lists/test?show_recipients=true');
-        done();
-      });
-    });
-  });
-
-  describe('all Method', function() {
-    it('should call client get method with the appropriate uri', function(done) {
-      var requestSpy = sinon.spy(SparkPost.prototype, 'get');
-
-      nock('https://api.sparkpost.com')
-        .get('/api/v1/recipient-lists')
-        .reply(200, { ok: true });
-
-      recipientLists.all(function(err, data) {
-        // need to make sure we called get method
-        expect(requestSpy.calledOnce).to.be.true;
-
-        // making sure the correct uri was constructed
-        expect(data.res.request.uri.href).to.equal('https://api.sparkpost.com:443/api/v1/recipient-lists');
-
-        SparkPost.prototype.get.restore(); // restoring function
+      recipientLists.find(options, function(err, data) {
+        expect(client.get.firstCall.args[0].qs).to.deep.equal({show_recipients: true});
         done();
       });
     });
   });
 
   describe('create Method', function() {
-    beforeEach(function() {
-      nock('https://api.sparkpost.com')
-        .post('/api/v1/recipient-lists')
-        .reply(200, {ok: true});
-    });
+    var test_list = [
+      {
+        address: {
+          email: 'test@test.com',
+          name: 'test'
+        }
+      }
+    ];
 
     it('should call client post method with the appropriate uri', function(done) {
-      var requestSpy = sinon.spy(SparkPost.prototype, 'post')
-        , list = [];
+      var options = {
+        list: test_list
+      };
 
+      recipientLists.create(options, function(err, data) {
+        expect(client.post.firstCall.args[0].uri).to.equal('recipient-lists');
+        done();
+      });
+    });
 
-      nock('https://api.sparkpost.com')
-        .post('/api/v1/recipient-lists')
-        .reply(200, {ok: true});
-
-      recipientLists.create(list, function(err, data) {
-        // need to make sure we called post method
-        expect(requestSpy.calledOnce).to.be.true;
-
-        // making sure the correct uri was constructed
-        expect(data.res.request.uri.href).to.equal('https://api.sparkpost.com:443/api/v1/recipient-lists');
-
-        SparkPost.prototype.post.restore(); // restoring function
+    it('should throw an error if id is missing', function(done) {
+      recipientLists.create(null, function(err) {
+        expect(err.message).to.equal('list is required');
+        expect(client.post).not.to.have.been.called;
         done();
       });
     });
 
     it('should allow num_rcpt_errors to be set in options', function(done) {
-      var list = []
-        , options = {
-          num_rcpt_errors: 3
-        };
+      var options = {
+        list: test_list,
+        num_rcpt_errors: 3
+      };
 
-      nock('https://api.sparkpost.com')
-        .post('/api/v1/recipient-lists?num_rcpt_errors=3')
-        .reply(200, {ok: true});
-
-      recipientLists.create(list, options, function(err, data) {
-        // making sure num_rcpt_errors was appended to the querystring
-        expect(data.res.request.uri.href).to.equal('https://api.sparkpost.com:443/api/v1/recipient-lists?num_rcpt_errors=3');
-
+      recipientLists.create(options, function(err, data) {
+        expect(client.post.firstCall.args[0].qs).to.deep.equal({num_rcpt_errors: 3});
         done();
       });
     });
@@ -141,23 +104,26 @@ describe('Recipient Lists Library', function() {
 
   describe('delete Method', function() {
     it('should call client delete method with the appropriate uri', function(done) {
-      var requestSpy = sinon.spy(SparkPost.prototype, 'delete');
-
-      nock('https://api.sparkpost.com')
-        .delete('/api/v1/recipient-lists/test')
-        .reply(200);
-
       recipientLists.delete('test', function(err, data) {
-        // need to make sure we called post method
-        expect(requestSpy.calledOnce).to.be.true;
+        expect(client['delete'].firstCall.args[0].uri).to.equal('recipient-lists/test');
+        done();
+      });
+    });
 
-        // making sure the correct uri was constructed
-        expect(data.res.request.uri.href).to.equal('https://api.sparkpost.com:443/api/v1/recipient-lists/test');
+    it('should throw an error if id is null', function(done) {
+      recipientLists['delete'](null, function(err) {
+        expect(err.message).to.equal('id is required');
+        expect(client['delete']).not.to.have.been.called;
+        done();
+      });
+    });
 
-        SparkPost.prototype['delete'].restore(); // restoring function
+    it('should throw an error if id is missing', function(done) {
+      recipientLists['delete'](function(err) {
+        expect(err.message).to.equal('id is required');
+        expect(client['delete']).not.to.have.been.called;
         done();
       });
     });
   });
-
 });
