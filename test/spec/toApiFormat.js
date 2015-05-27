1bdfe56528af310e5f1eec96b7835e6f3524124a
Benjamin Dean <ben@benjamindean.com>
2015-05-26 18:59:45 -0700

    Complex object toApiFormat should be working now

--- test/spec/toApiFormat.js
+++ test/spec/toApiFormat.js
@@ -37,33 +37,43 @@ describe('toApiFormatter', function() {
     });
   });
 
-  it('should convert array element object\'s keys to snake_case', function(done) {
+  it('should recursively convert complex object\'s keys to snake_case', function(done) {
     var testObj = {
-      foo: "foo"
-      , fooBar: "fooBar"
-      , fooBarBaz: "fooBarBaz"
-      , fizz: [
+      simpleString: "foo"
+      , boolVal: true 
+      , numBers: 201
+      , bangObj: {
+        strTwo: "bar"
+        , string_three: "skip"
+      }
+      , fizzArr: [
         {
-          buzzInga: "buzz",
-          bang: {
+          buzzInga: "buzz"
+          , bang: {
             bellHop: "bell"
           }
         }
       ]
+      , good_name: null
     };
 
     var validationObj = {
-      foo: "foo"
-      , foo_bar: "fooBar"
-      , foo_bar_baz: "fooBarBaz"
-      , fizz: [
+      simple_string: "foo"
+      , bool_val: true 
+      , num_bers: 201
+      , bang_obj: {
+        str_two: "bar"
+        , string_three: "skip"
+      }
+      , fizz_arr: [
         {
-          buzz_inga: "buzz",
-          bang: {
+          buzz_inga: "buzz"
+          , bang: {
             bell_hop: "bell"
           }
         }
       ]
+      , good_name: null
     };
 
     toApiFormatter( testObj, function( err, data ) {
