b2daba71ec9887c1f06e215729f1238036e2f558
Ewan Dennis <ewandennis@users.noreply.github.com>
2016-02-17 11:12:26 +0000

    Added message events docs and example code

--- examples/messageEvents/search_campaignClicks.js
+++ examples/messageEvents/search_campaignClicks.js
@@ -4,12 +4,8 @@ var key = 'YOURAPIKEY'
   , SparkPost = require('sparkpost')
   , client = new SparkPost(key)
   , searchParams = {
-      from: '2016-01-01T00:00',
-      to: '2016-01-02T23:59',
-      page: 1,
-      per_page: 5,
-      events: ['bounce', 'out_of_band'],
-      bounce_classes: [10]
+      events: 'click',
+      campaign_ids: 'monday_mailshot'
     };
 
 client.messageEvents.search(searchParams, function(err, res) {
