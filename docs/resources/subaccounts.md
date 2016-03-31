8bafacc8a0c59ad29fa55a5e1ba3ad7fc45e3dc2
Chris Charabaruk <chris.charabaruk@northcloud.com>
2016-03-31 17:26:16 -0400

    rewrote subaccounts library to match new API style

--- docs/resources/subaccounts.md
+++ docs/resources/subaccounts.md
@@ -9,17 +9,24 @@ This library provides easy access to the [Subaccounts](https://www.sparkpost.com
     * standard `callback(err, data)`
     * `err` - any error that occurred
     * `data` - full response from request client
-* **find(options, callback)**
+* **find(subaccountId, callback)**
   Retrieve details about a specified subaccount by its id
-  * `options.id` - the id of the subaccount you want to look up **required**
+  * `subaccountId` - the id of the subaccount you want to look up **required**
   * `callback` - see all function
 * **create(options, callback)**
   Create a new subaccount
-  * `options.subaccount` - a subaccount object **required**
+  * `options.name` - user-friendly name **required**
+  * `options.keyLabel` - user-friendly identifier for subaccount API key **required**
+  * `options.keyGrants` - list of grants to give the subaccount API key **required**
+  * `options.keyValidIps` - list of IPs the subaccount may be used from
+  * `options.ipPool` - id of the default IP pool assigned to subaccount's transmissions
   * `callback` - see all function
 * **update(options, callback)**
   Updates an existing subaccount
-  * `options.subaccount` - a subaccount object **required**
+  * `options.subaccountId` - the id of the subaccount you want to update **required**
+  * `options.name` - user-friendly name
+  * `options.status` - status of the subaccount
+  * `options.ipPool` - id of the default IP pool assigned to subaccount's transmissions
   * `callback` - see all function
 
 ## Examples
--- test/spec/subaccounts.speq.js
+++ test/spec/subaccounts.speq.js
@@ -29,18 +29,25 @@ describe('Subaccounts Library', function () {
 
   describe('find Method', function() {
     it('should call client get method with the appropriate uri', function(done) {
-      var options = {
-        id: 'test'
-      };
-      subaccounts.find(options, function(err, data) {
+      var subaccountId = 'test';
+
+      subaccounts.find(subaccountId, function(err, data) {
         expect(client.get.firstCall.args[0].uri).to.equal('subaccounts/test');
         done();
       });
     });
 
-    it('should throw an error if id is missing', function(done) {
+    it('should throw an error if subaccountId is null', function(done) {
       subaccounts.find(null, function(err) {
-        expect(err.message).to.equal('subaccount id is required');
+        expect(err.message).to.equal('subaccountId is required');
+        expect(client.get).not.to.have.been.called;
+        done();
+      });
+    });
+
+    it('should throw an error if subaccountId is missing', function(done) {
+      subaccounts.find(function(err) {
+        expect(err.message).to.equal('subaccountId is required');
         expect(client.get).not.to.have.been.called;
         done();
       });
@@ -50,9 +57,9 @@ describe('Subaccounts Library', function () {
   describe('create Method', function() {
     it('should call client post method with the appropriate uri', function(done) {
       var options = {
-        subaccount: {
-          id: 'test'
-        }
+        name: 'test',
+        keyLabel: 'test',
+        keyGrants: []
       };
 
       subaccounts.create(options, function(err, data) {
@@ -61,9 +68,56 @@ describe('Subaccounts Library', function () {
       });
     });
 
-    it('should throw an error if subaccount object is missing', function(done) {
+    it('should throw an error if options is null', function(done) {
       subaccounts.create(null, function(err) {
-        expect(err.message).to.equal('subaccount object is required');
+        expect(err.message).to.equal('options are required');
+        expect(client.post).not.to.have.been.called;
+        done();
+      });
+    });
+
+    it('should throw an error if options is missing', function(done) {
+      subaccounts.create(function(err) {
+        expect(err.message).to.equal('options are required');
+        expect(client.post).not.to.have.been.called;
+        done();
+      });
+    });
+
+    it('should throw an error if name is missing from options', function(done) {
+      var options = {
+        keyLabel: 'test',
+        keyGrants: []
+      };
+
+      subaccounts.create(options, function(err) {
+        expect(err.message).to.equal('name is required in options');
+        expect(client.post).not.to.have.been.called;
+        done();
+      });
+    });
+
+    it('should throw an error if keyLabel is missing from options', function(done) {
+      var options = {
+        name: 'test',
+        keyGrants: []
+      };
+
+      subaccounts.create(options, function(err) {
+        expect(err.message).to.equal('keyLabel is required in options');
+        expect(client.post).not.to.have.been.called;
+        done();
+      });
+    });
+
+    it('should throw an error if keyGrants is missing from options', function(done) {
+      var options = {
+        name: 'test',
+        keyLabel: 'test'
+      };
+
+      subaccounts.create(options, function(err) {
+        expect(err.message).to.equal('keyGrants is required in options');
         expect(client.post).not.to.have.been.called;
         done();
       });
@@ -73,9 +127,7 @@ describe('Subaccounts Library', function () {
   describe('update Method', function() {
     it('should call client put method with the appropriate uri', function(done) {
       var options = {
-        subaccount: {
-          id: 'test'
-        }
+        subaccountId: 'test'
       };
 
       subaccounts.update(options, function(err, data) {
@@ -84,9 +136,27 @@ describe('Subaccounts Library', function () {
       });
     });
 
-    it('should throw an error if subaccount object is missing', function(done) {
+    it('should throw an error if options is null', function(done) {
       subaccounts.update(null, function(err) {
-        expect(err.message).to.equal('subaccount object is required');
+        expect(err.message).to.equal('options are required');
+        expect(client.put).not.to.have.been.called;
+        done();
+      });
+    });
+
+    it('should throw an error if options is missing', function(done) {
+      subaccounts.update(function(err) {
+        expect(err.message).to.equal('options are required');
+        expect(client.put).not.to.have.been.called;
+        done();
+      });
+    });
+
+    it('should throw an error if subaccountId is missing from options', function(done) {
+      var options = {};
+
+      subaccounts.update(options, function(err, data) {
+        expect(err.message).to.equal('subaccountId is required in options');
         expect(client.put).not.to.have.been.called;
         done();
       });
