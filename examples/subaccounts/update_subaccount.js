440cd15571f03153e980786beca46bda10e2721d
Chris Charabaruk <chris.charabaruk@outlook.com>
2016-04-27 09:37:41 -0400

    add examples of subaccounts resource use (#143)
    * add examples of subaccounts resource use
    
    * Change "SDK" to "client library" in subaccount examples

---
+++ examples/subaccounts/update_subaccount.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var key = 'YOURAPIKEY'
+  , SparkPost = require('sparkpost')
+  , client = new SparkPost(key)
+  , options = {
+    subaccountId: 123
+    , name: 'Test Subaccount'
+    , status: 'suspended'
+  };
+
+client.subaccounts.update(options, function(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res.body);
+    console.log('Congrats you can use our client library!');
+  }
+});
