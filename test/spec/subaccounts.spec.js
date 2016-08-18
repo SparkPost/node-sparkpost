5438cccd823a6cf679fab48426c2bc15b53dbc2b
Aydrian J. Howard <aydrian@gmail.com>
2016-08-18 15:55:19 -0400

    updated subaccounts for promise support

--- test/spec/subaccounts.spec.js
+++ test/spec/subaccounts.spec.js
@@ -1,7 +1,9 @@
 var chai = require('chai')
   , expect = chai.expect
   , sinon = require('sinon')
-  , sinonChai = require('sinon-chai');
+  , sinonChai = require('sinon-chai')
+  /* global -Promise */
+  , Promise = require('bluebird');
 
 chai.use(sinonChai);
 
@@ -10,9 +12,9 @@ describe('Subaccounts Library', function () {
 
   beforeEach(function() {
     client = {
-      get: sinon.stub().yields(),
-      post: sinon.stub().yields(),
-      put: sinon.stub().yields()
+      get: sinon.stub().returns(Promise.resolve({})),
+      post: sinon.stub().returns(Promise.resolve({})),
+      put: sinon.stub().returns(Promise.resolve({}))
     };
 
     subaccounts = require('../../lib/subaccounts')(client);
