01123c0bf6e6b196124d1c71dded14044c6a7b30
Aydrian J. Howard <aydrian@gmail.com>
2016-08-18 15:32:09 -0400

    updated inboundDomains for promise support

--- test/spec/inboundDomains.spec.js
+++ test/spec/inboundDomains.spec.js
@@ -1,7 +1,9 @@
 var chai = require('chai')
   , expect = chai.expect
   , sinon = require('sinon')
-  , sinonChai = require('sinon-chai');
+  , sinonChai = require('sinon-chai')
+  /* global -Promise */
+  , Promise = require('bluebird');
 
 chai.use(sinonChai);
 
@@ -10,10 +12,9 @@ describe('Inbound Domains Library', function() {
 
   beforeEach(function() {
     client = {
-      get: sinon.stub().yields(),
-      post: sinon.stub().yields(),
-      delete: sinon.stub().yields()
-
+      get: sinon.stub().returns(Promise.resolve({})),
+      post: sinon.stub().returns(Promise.resolve({})),
+      delete: sinon.stub().returns(Promise.resolve({}))
     };
 
     inboundDomains = require('../../lib/inboundDomains')(client);
