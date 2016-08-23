d399913f39ede35d19dbf4400a7eba39072f2c53
Aydrian J. Howard <aydrian@gmail.com>
2016-08-23 16:59:25 -0400

    removed testing and coverage from grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,11 @@
-var matchdep = require('matchdep')
-  , gruntTimer = require('time-grunt')
-  , path = require('path');
+var path = require('path');
 
 module.exports = function(grunt) {
-  // Dynamically load any preexisting grunt tasks/modules
-  matchdep.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
-  // Pretty timing on grunt commands
-  gruntTimer(grunt);
-
   var config = {
-    binPath: path.join('.', 'node_modules', '.bin'),
+    binPath: path.join('.', 'node_modules', '.bin')
   };
 
-  var reporter = grunt.option('reporter') || 'xunit-file';
+  grunt.loadNpmTasks('grunt-bump');
 
   // Configure existing grunt tasks and create custom ones
   grunt.initConfig({
@@ -33,34 +25,9 @@ module.exports = function(grunt) {
         , pushTo: 'upstream'
         , gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
       }
-    },
-    shell: {
-      lint: {
-        command: 'npm run lint'
-      },
-      coverage: {
-        command: path.join(config.binPath, 'istanbul') + ' cover --report lcov --dir test/reports/ node_modules/mocha/bin/_mocha test/spec -- --reporter ' + reporter,
-        options: {
-          stdout: true,
-          failOnError: true
-        }
-      },
-      test: {
-        command: path.join(config.binPath, '_mocha') + ' test/spec'
-      }
     }
   });
 
-  // grunt lint - leverages grunt-contrib-jshint command, lints our code
-  grunt.registerTask('lint', [ 'shell:lint' ]);
-
-  // grunt test - runs linting and then our unit tests
-  grunt.registerTask('test', [
-    'lint',
-    'shell:test',
-    'shell:coverage'
-  ]);
-
-  // register default grunt command as grunt test
-  grunt.registerTask('default', [ 'test' ]);
+  // register default grunt command
+  grunt.registerTask('default', []);
 };
