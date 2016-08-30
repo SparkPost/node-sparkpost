3c20131b3e951bb3242d52e036758189856772d1
Aydrian J. Howard <aydrian@gmail.com>
2016-08-30 16:54:50 -0400

    Updated webhooks doc and examples

--- examples/webhooks/describe_webhook.js
+++ examples/webhooks/describe_webhook.js
@@ -4,15 +4,27 @@ var key = 'YOURAPIKEY'
   , SparkPost = require('sparkpost')
   , client = new SparkPost(key)
   , options = {
-    id: 'TEST_WEBHOOK_UUID'
-    , timezone: 'America/New_York'
+    id: 'TEST_WEBHOOK_UUID',
+    timezone: 'America/New_York'
   };
 
+client.webhooks.describe(options)
+  .then(data => {
+    console.log('Congrats you can use our client library!');
+    console.log(data);
+  })
+  .catch(err => {
+    console.log('Whoops! Something went wrong');
+    console.log(err);
+  });
+
+// Using a callback
 client.webhooks.describe(options, function(err, data) {
   if (err) {
+    console.log('Whoops! Something went wrong');
     console.log(err);
   } else {
-    console.log(data);
     console.log('Congrats you can use our client library!');
+    console.log(data);
   }
 });
--- examples/webhooks/update_webhook.js
+++ examples/webhooks/update_webhook.js
@@ -4,19 +4,31 @@ var key = 'YOURAPIKEY'
   , SparkPost = require('sparkpost')
   , client = new SparkPost(key)
   , webhook = {
-    id: 'TEST_WEBHOOK_UUID'
-    , name: 'Renamed Test webhook'
-    , events: [
+    id: 'TEST_WEBHOOK_UUID',
+    name: 'Renamed Test Webhook',
+    events: [
       'policy_rejection',
       'delay'
     ]
   };
 
+client.webhooks.update(webhook)
+  .then(data => {
+    console.log('Congrats you can use our client library!');
+    console.log(data);
+  })
+  .catch(err => {
+    console.log('Whoops! Something went wrong');
+    console.log(err);
+  });
+
+// Using a callback
 client.webhooks.update(webhook, function(err, data) {
   if (err) {
+    console.log('Whoops! Something went wrong');
     console.log(err);
   } else {
-    console.log(data);
     console.log('Congrats you can use our client library!');
+    console.log(data);
   }
 });
