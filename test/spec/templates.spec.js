2c70cad1c9524c61d7d88dd987779e0761717745
Aydrian J. Howard <aydrian@gmail.com>
2016-08-30 15:02:04 -0400

    Updated templates lib and tests

--- test/spec/templates.spec.js
+++ test/spec/templates.spec.js
@@ -1,180 +1,153 @@
+'use strict';
+
 var chai = require('chai')
   , expect = chai.expect
-  , sinon = require('sinon')
-  , sinonChai = require('sinon-chai')
-  , Promise = require('../../lib/Promise');
+  , sinon = require('sinon');
 
-chai.use(sinonChai);
+require('sinon-as-promised');
+
+chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised'));
 
 describe('Templates Library', function() {
   var client, templates;
 
   beforeEach(function() {
     client = {
-      get: sinon.stub().returns(Promise.resolve({})),
-      post: sinon.stub().returns(Promise.resolve({})),
-      put: sinon.stub().returns(Promise.resolve({})),
-      delete: sinon.stub().returns(Promise.resolve({}))
+      get: sinon.stub().resolves({}),
+      post: sinon.stub().resolves({}),
+      put: sinon.stub().resolves({}),
+      delete: sinon.stub().resolves({})
     };
 
     templates = require('../../lib/templates')(client);
   });
 
   describe('all Method', function() {
-    it('should call client get method with the appropriate uri', function(done) {
-      templates.all(function(err, data) {
-        expect(client.get.firstCall.args[0].uri).to.equal('templates');
-        done();
-      });
+    it('should call client get method with the appropriate uri', function() {
+      templates.all()
+        .then(function() {
+          expect(client.get.firstCall.args[0].uri).to.equal('templates');
+        });
     });
   });
 
   describe('find Method', function() {
-    it('should call client get method with the appropriate uri', function(done) {
+    it('should call client get method with the appropriate uri', function() {
       var options = {
         id: 'test'
       };
-      templates.find(options, function(err, data) {
-        expect(client.get.firstCall.args[0].uri).to.equal('templates/test');
-        done();
-      });
+      templates.find(options)
+        .then(function() {
+          expect(client.get.firstCall.args[0].uri).to.equal('templates/test');
+        });
     });
 
-    it('should throw an error if id is missing', function(done) {
-      templates.find(null, function(err) {
-        expect(err.message).to.equal('template id is required');
-        expect(client.get).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if id is missing', function() {
+      return expect(templates.find()).to.be.rejectedWith('template id is required');
     });
 
-    it('should allow draft to be set in options', function(done) {
+    it('should allow draft to be set in options', function() {
       var options = {
         id: 'test',
         draft: true
       };
 
-      templates.find(options, function(err, data) {
-        expect(client.get.firstCall.args[0].qs).to.deep.equal({draft: true});
-        done();
-      });
+      templates.find(options)
+        .then(function() {
+          expect(client.get.firstCall.args[0].qs).to.deep.equal({draft: true});
+        });
     });
   });
 
   describe('create Method', function() {
-    it('should call client post method with the appropriate uri', function(done) {
-      var options = {
-        template: {
-          id: "test"
-        }
+    it('should call client post method with the appropriate uri', function() {
+      var template = {
+        id: 'test'
       };
 
-      templates.create(options, function(err, data) {
-        expect(client.post.firstCall.args[0].uri).to.equal('templates');
-        done();
-      });
+      templates.create(template)
+        .then(function() {
+          expect(client.post.firstCall.args[0].uri).to.equal('templates');
+        });
     });
 
-    it('should throw an error if id is missing', function(done) {
-      templates.create(null, function(err) {
-        expect(err.message).to.equal('template object is required');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if template object is missing', function() {
+      return expect(templates.create()).to.be.rejectedWith('template object is required');
     });
   });
 
   describe('update Method', function() {
-    it('should call client put method with the appropriate uri', function(done) {
-      var options = {
-        template: {
-          id: "test"
-        }
+    it('should call client put method with the appropriate uri', function() {
+      var template = {
+        id: 'test'
       };
 
-      templates.update(options, function(err, data) {
-        expect(client.put.firstCall.args[0].uri).to.equal('templates/test');
-        done();
-      });
+      templates.update(template)
+        .then(function() {
+          expect(client.put.firstCall.args[0].uri).to.equal('templates/test');
+        });
     });
 
-    it('should throw an error if id is missing', function(done) {
-      templates.update(null, function(err) {
-        expect(err.message).to.equal('template object is required');
-        expect(client.put).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if template object is missing', function() {
+      return expect(templates.update()).to.be.rejectedWith('template object is required');
     });
 
-    it('should allow update_published to be set in options', function(done) {
-      var options = {
-        template: {
-          id: "test"
-        },
+    it('should throw an error if template id is missing', function() {
+      return expect(templates.update({})).to.be.rejectedWith('template id is required');
+    });
+
+    it('should allow update_published to be set in options', function() {
+      var template = {
+        id: 'test',
         update_published: true
       };
 
-      templates.update(options, function(err, data) {
-        expect(client.put.firstCall.args[0].qs).to.deep.equal({update_published: true});
-        done();
-      });
+      templates.update(template)
+        .then(function() {
+          expect(client.put.firstCall.args[0].qs).to.deep.equal({update_published: true});
+        });
     });
   });
 
   describe('delete Method', function() {
-    it('should call client delete method with the appropriate uri', function(done) {
-      templates['delete']('test', function(err, data) {
-        expect(client['delete'].firstCall.args[0].uri).to.equal('templates/test');
-        done();
-      });
+    it('should call client delete method with the appropriate uri', function() {
+      templates.delete('test')
+        .then(function() {
+          expect(client.delete.firstCall.args[0].uri).to.equal('templates/test');
+        });
     });
 
-    it('should throw an error if id is null', function(done) {
-      templates['delete'](null, function(err) {
-        expect(err.message).to.equal('template id is required');
-        expect(client['delete']).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if id is missing', function(done) {
-      templates['delete'](function(err) {
-        expect(err.message).to.equal('template id is required');
-        expect(client['delete']).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if id is missing', function() {
+      return expect(templates.delete()).to.be.rejectedWith('template id is required');
     });
   });
 
   describe('preview Method', function() {
-    it('should call client post method with the appropriate uri', function(done) {
+    it('should call client post method with the appropriate uri', function() {
       var options = {
         id: 'test'
       };
-      templates.preview(options, function(err, data) {
-        expect(client.post.firstCall.args[0].uri).to.equal('templates/test/preview');
-        done();
-      });
+      templates.preview(options)
+        .then(function() {
+          expect(client.post.firstCall.args[0].uri).to.equal('templates/test/preview');
+        });
     });
 
-    it('should throw an error if id is missing', function(done) {
-      templates.preview(null, function(err) {
-        expect(err.message).to.equal('template id is required');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if id is missing', function() {
+      return expect(templates.preview()).to.be.rejectedWith('template id is required');
     });
 
-    it('should allow draft to be set in options', function(done) {
+    it('should allow draft to be set in options', function() {
       var options = {
         id: 'test',
         draft: true
       };
 
-      templates.preview(options, function(err, data) {
-        expect(client.post.firstCall.args[0].qs).to.deep.equal({draft: true});
-        done();
-      });
+      templates.preview(options)
+        .then(function() {
+          expect(client.post.firstCall.args[0].qs).to.deep.equal({draft: true});
+        });
     });
   });
 });
