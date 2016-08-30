062f57b35317b5c1d4f664e0691961391866dbad
Aydrian J. Howard <aydrian@gmail.com>
2016-08-30 09:56:55 -0400

    Updated sending domains lib and tests

--- test/spec/sendingDomains.spec.js
+++ test/spec/sendingDomains.spec.js
@@ -1,202 +1,140 @@
+'use strict';
+
 var chai = require('chai')
   , expect = chai.expect
-  , sinon = require('sinon')
-  , sinonChai = require('sinon-chai')
-  , Promise = require('../../lib/Promise');
+  , sinon = require('sinon');
 
-chai.use(sinonChai);
+require('sinon-as-promised');
+
+chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised'));
 
 describe('Sending Domains Library', function() {
   var client, sendingDomains;
 
   beforeEach(function() {
     client = {
-      get: sinon.stub().returns(Promise.resolve({})),
-      post: sinon.stub().returns(Promise.resolve({})),
-      put: sinon.stub().returns(Promise.resolve({})),
-      delete: sinon.stub().returns(Promise.resolve({}))
+      get: sinon.stub().resolves({}),
+      post: sinon.stub().resolves({}),
+      put: sinon.stub().resolves({}),
+      delete: sinon.stub().resolves({})
     };
 
     sendingDomains = require('../../lib/sendingDomains')(client);
   });
 
   describe('all Method', function() {
-    it('should call client get method with the appropriate uri', function(done) {
-      sendingDomains.all().then(function() {
-        expect(client.get.firstCall.args[0]).to.deep.equal({uri:'sending-domains'});
-        done();
-      });
+    it('should call client get method with the appropriate uri', function() {
+      sendingDomains.all()
+        .then(function() {
+          expect(client.get.firstCall.args[0]).to.deep.equal({uri: 'sending-domains'});
+        });
     });
   });
 
   describe('find Method', function() {
-    it('should call client get method with the appropriate uri', function(done) {
-      sendingDomains.find('test').then(function() {
-        expect(client.get.firstCall.args[0]).to.deep.equal({uri: 'sending-domains/test'});
-        done();
-      });
+    it('should call client get method with the appropriate uri', function() {
+      sendingDomains.find('test')
+        .then(function() {
+          expect(client.get.firstCall.args[0]).to.deep.equal({uri: 'sending-domains/test'});
+        });
     });
 
-    it('should throw an error if domain is null', function(done) {
-      sendingDomains.find(null).catch(function(err) {
-        expect(err.message).to.equal('domain is required');
-        expect(client.get).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if domain is missing', function(done) {
-      sendingDomains.find(function(err) {
-        expect(err.message).to.equal('domain is required');
-        expect(client.get).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if domain is missing', function() {
+      return expect(sendingDomains.find()).to.be.rejectedWith('domain is required');
     });
   });
 
   describe('create Method', function() {
-    it('should call client post method with the appropriate uri', function(done) {
-      var domainBody = {
-        domain: "test"
+    it('should call client post method with the appropriate uri', function() {
+      var sendingDomain = {
+        domain: 'test'
       };
 
-      sendingDomains.create(domainBody).then(function(data) {
+      sendingDomains.create(sendingDomain).then(function() {
         expect(client.post.firstCall.args[0].uri).to.equal('sending-domains');
-        done();
       });
     });
 
-    it('should throw an error if domainBody is null', function(done) {
-      sendingDomains.create(null, function(err) {
-        expect(err.message).to.equal('domainBody is required');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if domainBody is missing', function(done) {
-      sendingDomains.create(function(err) {
-        expect(err.message).to.equal('domainBody is required');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if domain is missing from domainBody', function(done) {
-      sendingDomains.create({}, function(err){
-        expect(err.message).to.equal('domain is required in the domainBody');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if sendingDomain is missing', function() {
+      return expect(sendingDomains.create()).to.be.rejectedWith('sending domain is required');
     });
   });
 
   describe('update Method', function() {
-    it('should call client put method with the appropriate uri', function(done) {
-      var domainBody = {
-        domain: "test"
+    it('should call client put method with the appropriate uri', function() {
+      var sendingDomain = {
+        domain: 'test'
       };
 
-      sendingDomains.update(domainBody, function(err, data) {
-        expect(client.put.firstCall.args[0].uri).to.equal('sending-domains/test');
-        done();
-      });
+      sendingDomains.update(sendingDomain)
+        .then(function() {
+          expect(client.put.firstCall.args[0].uri).to.equal('sending-domains/test');
+        });
     });
 
-    it('should throw an error if domainBody is null', function(done) {
-      sendingDomains.update(null, function(err) {
-        expect(err.message).to.equal('domainBody is required');
-        expect(client.put).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if sendingDomain is missing', function() {
+      return expect(sendingDomains.update()).to.be.rejectedWith('sending domain is required');
     });
 
-    it('should throw an error if domainBody is missing', function(done) {
-      sendingDomains.update(function(err) {
-        expect(err.message).to.equal('domainBody is required');
-        expect(client.put).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if domain is missing from domainBody', function(done) {
-      sendingDomains.update({}, function(err){
-        expect(err.message).to.equal('domain is required in the domainBody');
-        expect(client.put).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if domain is missing from sendingDomain', function() {
+      return expect(sendingDomains.update({})).to.be.rejectedWith('sendingDomain.domain is required');
     });
   });
 
   describe('delete Method', function() {
-    it('should call client delete method with the appropriate uri', function(done) {
-      sendingDomains.delete('test', function(err, data) {
-        expect(client.delete.firstCall.args[0].uri).to.equal('sending-domains/test');
-        done();
-      });
+    it('should call client delete method with the appropriate uri', function() {
+      sendingDomains.delete('test')
+        .then(function() {
+          expect(client.delete.firstCall.args[0].uri).to.equal('sending-domains/test');
+        });
     });
 
-    it('should throw an error if domain is null', function(done) {
-      sendingDomains.delete(null, function(err) {
-        expect(err.message).to.equal('domain is required');
-        expect(client.delete).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if domain is missing', function(done) {
-      sendingDomains.delete(function(err) {
-        expect(err.message).to.equal('domain is required');
-        expect(client.delete).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if domain is missing', function() {
+      return expect(sendingDomains.delete()).to.be.rejectedWith('domain is required');
     });
   });
 
   describe('verify Method', function() {
-    it('should call client post method with the appropriate uri', function(done) {
+    it('should call client post method with the appropriate uri', function() {
       var options = {
         domain: 'test'
       };
 
-      sendingDomains.verify(options, function() {
-        expect(client.post.firstCall.args[0].uri).to.equal('sending-domains/test/verify');
-        done();
-      });
+      sendingDomains.verify(options)
+        .then(function() {
+          expect(client.post.firstCall.args[0].uri).to.equal('sending-domains/test/verify');
+        });
     });
 
-    it('should throw an error if domain is missing', function(done) {
-      sendingDomains.verify(null, function(err) {
-        expect(err.message).to.equal('domain is required');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if domain is missing', function() {
+      return expect(sendingDomains.verify({})).to.be.rejectedWith('domain is required');
     });
 
-    it('should default verifyDKIM and verifySPF to be true', function(done) {
+    it('should default verifyDKIM and verifySPF to be true', function() {
       var options = {
         domain: 'test'
       };
 
-      sendingDomains.verify(options, function() {
-        expect(client.post.firstCall.args[0].json.dkim_verify).to.be.true;
-        expect(client.post.firstCall.args[0].json.spf_verify).to.be.true;
-        done();
-      });
+      sendingDomains.verify(options)
+        .then(function() {
+          expect(client.post.firstCall.args[0].json.dkim_verify).to.be.true;
+          expect(client.post.firstCall.args[0].json.spf_verify).to.be.true;
+        });
     });
 
-    it('should allow a user to set verifyDKIM and verifySPF', function(done){
+    it('should allow a user to set verifyDKIM and verifySPF', function() {
       var options = {
         domain: 'test',
         verifyDKIM: false,
         verifySPF: false
       };
 
-      sendingDomains.verify(options, function() {
-        expect(client.post.firstCall.args[0].json.dkim_verify).to.be.false;
-        expect(client.post.firstCall.args[0].json.spf_verify).to.be.false;
-        done();
-      });
+      sendingDomains.verify(options)
+        .then(function() {
+          expect(client.post.firstCall.args[0].json.dkim_verify).to.be.false;
+          expect(client.post.firstCall.args[0].json.spf_verify).to.be.false;
+        });
     });
   });
 });
