443a776db5138d63bf3e3336112802d66b5387db
Aydrian J. Howard <aydrian@gmail.com>
2016-08-31 09:34:19 -0400

    Updated subaccounts lib and tests

--- test/spec/subaccounts.spec.js
+++ test/spec/subaccounts.spec.js
@@ -1,221 +1,86 @@
+'use strict';
+
 var chai = require('chai')
   , expect = chai.expect
-  , sinon = require('sinon')
-  , sinonChai = require('sinon-chai')
-  , Promise = require('../../lib/Promise');
+  , sinon = require('sinon');
 
-chai.use(sinonChai);
+require('sinon-as-promised');
 
-describe('Subaccounts Library', function () {
+chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised'));
+
+describe('Subaccounts Library', function() {
   var client, subaccounts;
 
   beforeEach(function() {
     client = {
-      get: sinon.stub().returns(Promise.resolve({})),
-      post: sinon.stub().returns(Promise.resolve({})),
-      put: sinon.stub().returns(Promise.resolve({}))
+      get: sinon.stub().resolves({}),
+      post: sinon.stub().resolves({}),
+      put: sinon.stub().resolves({})
     };
 
     subaccounts = require('../../lib/subaccounts')(client);
   });
 
   describe('all Method', function() {
-    it('should call client get method with the appropriate uri', function(done) {
-      subaccounts.all(function(err, data) {
-        expect(client.get.firstCall.args[0].uri).to.equal('subaccounts');
-        done();
-      });
+    it('should call client get method with the appropriate uri', function() {
+      subaccounts.all()
+        .then(function() {
+          expect(client.get.firstCall.args[0].uri).to.equal('subaccounts');
+        });
     });
   });
 
   describe('find Method', function() {
-    it('should call client get method with the appropriate uri', function(done) {
-      var subaccountId = 'test';
-
-      subaccounts.find(subaccountId, function(err, data) {
-        expect(client.get.firstCall.args[0].uri).to.equal('subaccounts/test');
-        done();
-      });
+    it('should call client get method with the appropriate uri', function() {
+      subaccounts.find('test')
+        .then(function() {
+          expect(client.get.firstCall.args[0].uri).to.equal('subaccounts/test');
+        });
     });
 
-    it('should throw an error if subaccountId is null', function(done) {
-      subaccounts.find(null, function(err) {
-        expect(err.message).to.equal('subaccountId is required');
-        expect(client.get).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if subaccountId is missing', function(done) {
-      subaccounts.find(function(err) {
-        expect(err.message).to.equal('subaccountId is required');
-        expect(client.get).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if id is missing', function() {
+      return expect(subaccounts.find()).to.be.rejectedWith('id is required');
     });
   });
 
   describe('create Method', function() {
-    it('should call client post method with the appropriate uri', function(done) {
-      var options = {
+    it('should call client post method with the appropriate uri', function() {
+      var subaccount = {
         name: 'test',
-        keyLabel: 'test',
-        keyGrants: []
+        key_label: 'test',
+        key_grants: []
       };
 
-      subaccounts.create(options, function(err, data) {
-        expect(client.post.firstCall.args[0].uri).to.equal('subaccounts');
-        done();
-      });
+      subaccounts.create(subaccount)
+        .then(function() {
+          expect(client.post.firstCall.args[0].uri).to.equal('subaccounts');
+        });
     });
 
-    it('should throw an error if options is null', function(done) {
-      subaccounts.create(null, function(err) {
-        expect(err.message).to.equal('options are required');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if options is missing', function(done) {
-      subaccounts.create(function(err) {
-        expect(err.message).to.equal('options are required');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if name is missing from options', function(done) {
-      var options = {
-        keyLabel: 'test',
-        keyGrants: []
-      };
-
-      subaccounts.create(options, function(err) {
-        expect(err.message).to.equal('name is required in options');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if keyLabel is missing from options', function(done) {
-      var options = {
-        name: 'test',
-        keyGrants: []
-      };
-
-      subaccounts.create(options, function(err) {
-        expect(err.message).to.equal('keyLabel is required in options');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if keyGrants is missing from options', function(done) {
-      var options = {
-        name: 'test',
-        keyLabel: 'test'
-      };
-
-      subaccounts.create(options, function(err) {
-        expect(err.message).to.equal('keyGrants is required in options');
-        expect(client.post).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should not set key_valid_ips in request if keyValidIps is missing from options', function(done) {
-      var options = {
-        name: 'test',
-        keyLabel: 'test',
-        keyGrants: []
-      };
-
-      subaccounts.create(options, function(err, data) {
-        expect(client.post.firstCall.args[0].json.key_valid_ips).to.be.undefined;
-        done();
-      })
-    });
-
-    it('should not set key_valid_ips in request if keyValidIps is empty array', function(done) {
-      var options = {
-        name: 'test',
-        keyLabel: 'test',
-        keyGrants: [],
-        keyValidIps: []
-      };
-
-      subaccounts.create(options, function(err, data) {
-        expect(client.post.firstCall.args[0].json.key_valid_ips).to.be.undefined;
-        done();
-      })
-    });
-
-    it('should set key_valid_ips in request if keyValidIps is in options and is a non-empty array', function(done) {
-      var options = {
-        name: 'test',
-        keyLabel: 'test',
-        keyGrants: [],
-        keyValidIps: ['127.0.0.1']
-      };
-
-      subaccounts.create(options, function(err, data) {
-        expect(client.post.firstCall.args[0].json.key_valid_ips).to.eql(['127.0.0.1']);
-        done();
-      })
-    });
-
-    it('should set key_valid_ips in request if keyValidIps is in options and is not an array', function(done) {
-      var options = {
-        name: 'test',
-        keyLabel: 'test',
-        keyGrants: [],
-        keyValidIps: '127.0.0.1'
-      };
-
-      subaccounts.create(options, function(err, data) {
-        expect(client.post.firstCall.args[0].json.key_valid_ips).to.eql(['127.0.0.1']);
-        done();
-      })
+    it('should throw an error if subaccount object is missing', function() {
+      return expect(subaccounts.create()).to.be.rejectedWith('subaccount object is required');
     });
   });
 
   describe('update Method', function() {
-    it('should call client put method with the appropriate uri', function(done) {
-      var options = {
-        subaccountId: 'test'
+    it('should call client put method with the appropriate uri', function() {
+      var subaccount = {
+        id: 'test'
       };
 
-      subaccounts.update(options, function(err, data) {
-        expect(client.put.firstCall.args[0].uri).to.equal('subaccounts/test');
-        done();
-      });
+      subaccounts.update(subaccount)
+        .then(function() {
+          expect(client.put.firstCall.args[0].uri).to.equal('subaccounts/test');
+        });
     });
 
-    it('should throw an error if options is null', function(done) {
-      subaccounts.update(null, function(err) {
-        expect(err.message).to.equal('options are required');
-        expect(client.put).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if subaccount object is missing', function() {
+      return expect(subaccounts.update()).to.be.rejectedWith('subaccount object is required');
     });
 
-    it('should throw an error if options is missing', function(done) {
-      subaccounts.update(function(err) {
-        expect(err.message).to.equal('options are required');
-        expect(client.put).not.to.have.been.called;
-        done();
-      });
-    });
-
-    it('should throw an error if subaccountId is missing from options', function(done) {
-      var options = {};
-
-      subaccounts.update(options, function(err, data) {
-        expect(err.message).to.equal('subaccountId is required in options');
-        expect(client.put).not.to.have.been.called;
-        done();
-      });
+    it('should throw an error if subaccountId is missing from options', function() {
+      return expect(subaccounts.update({})).to.be.rejectedWith('subaccount.id is required');
     });
   });
 });
